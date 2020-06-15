d4b470a1c5181975e15dcde22862febfc3abec1d
Ubuntu <ubuntu@infra-server.f5-udf.com>
2020-06-15 15:35:26 +0200

    arcadia on unit and kic now working

---
+++ arcadia-unit-kic/1.docker-images-preparation/README.md
@@ -0,0 +1,8 @@
+# Arcadia Finance
+
+Arcadia Finance Application in Nginx Unit
+
+![picture](https://gitlab.com/MattDierick/arcadia-finance/raw/master/Micro%20Services%20architecture.png)
+
+Credentials are admin/iloveblue
+
--- arcadia-unit-kic/1.docker-images-preparation/buildArcadia.sh
+++ arcadia-unit-kic/1.docker-images-preparation/buildArcadia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for dir in {main,bakend} 
+for Adir in {main,bakend,app3,app3} 
 do 
     cd ~/f5-arcadia/arcadia-unit-kic/1.docker-images-preparation/$dir
     docker build . -t "registry.f5-udf.com:5000/arcadia-${dir}:unit"
